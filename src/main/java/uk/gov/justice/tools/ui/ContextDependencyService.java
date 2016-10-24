2cbbb6ab43ab11faabbdd989c3877c6516f49d13
satishk.dv2 <satish.kumar@hmcts.net>
2016-10-24 15:39:07 +0100

    CCR-3860 moved app files to static folder

--- src/main/java/uk/gov/justice/tools/ui/ContextDependencyService.java
+++ src/main/java/uk/gov/justice/tools/ui/ContextDependencyService.java
@@ -2,11 +2,17 @@ package uk.gov.justice.tools.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ContextDependencyService {
+public class ContextDependencyService extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContextDependencyService.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ContextDependencyService.class);
 	}
 }
