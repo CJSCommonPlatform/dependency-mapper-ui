aa6fb29d1f28d24552daeccb70de89c5ddfefb41
Hasan.shaik <hasan.shaik@ext.soprasteria.com>
2016-10-28 08:08:15 +0100

    Including yml config

--- src/main/java/uk/gov/justice/tools/ui/ContextDependencyServiceBoot.java
+++ src/main/java/uk/gov/justice/tools/ui/ContextDependencyServiceBoot.java
@@ -8,24 +8,18 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class ContextDependencyServiceBoot extends Application<Configuration> {
-    //TODO : requires refactoring here, Suggested option would be one application.yml file having all properties defined there
 
     static String DEFAULT_RAML_REPORT_DIR = "/opt/raml-reports/";
     static String RAML_REPORT_DIR = System.getProperty("ramlReportDir", DEFAULT_RAML_REPORT_DIR);
     static String DEFAULT_FILE_URL = "/opt/contexts.json";
     static String FILE_URL = System.getProperty("filePath", DEFAULT_FILE_URL);
 
-    static {
-        if (null == System.getProperty("dw.server.applicationConnectors[0].port")) {
-            System.setProperty("dw.server.applicationConnectors[0].port", "9999");
-        }
-    }
-    //END TODO
+
 
     private final UIConfig uiConfig = new UIConfig();
 
     public static void main(String[] args) throws Exception {
-        new ContextDependencyServiceBoot().run(args);
+        new ContextDependencyServiceBoot().run(new String[] {"server", "src/main/resources/configuration.yaml"});
     }
 
     @Override
@@ -34,6 +28,7 @@ public class ContextDependencyServiceBoot extends Application<Configuration> {
         uiConfig.setRamlReportDir(RAML_REPORT_DIR);
         bootstrap.addBundle(new AssetsBundle("/static", "/contextual", "index.html", "static"));
         //bootstrap.addBundle(new AssetsBundle("file:" +RAML_REPORT_DIR, "/raml-report*//**"));
+     
     }
 
     @Override
