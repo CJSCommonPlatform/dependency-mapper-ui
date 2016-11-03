292bfea13ba9c9864518abebb9b64d6ee6e65cae
hshaik <hasan.shaik@ext.soprasteria.com>
2016-11-03 15:36:15 +0000

    Adding raml documentation index.html

--- src/main/java/uk/gov/justice/tools/ui/RamlStaticFileService.java
+++ src/main/java/uk/gov/justice/tools/ui/RamlStaticFileService.java
@@ -2,18 +2,14 @@ package uk.gov.justice.tools.ui;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Path(value = "/ramlReport")
 @Produces(MediaType.TEXT_HTML)
@@ -33,18 +29,4 @@ public class RamlStaticFileService {
         return new String(Files.readAllBytes(Paths.get(uiConfig.getRamlReportDir().concat(ramlFileName))));
 
     }
-
-//    public static void main(String[] args) throws IOException {
-//        RamlStaticFileService ramlStaticFileService = new RamlStaticFileService(null);
-//        List<String> results = new ArrayList<String>();
-//        File[] files = new File("D:\\MOJ\\dependency-mapper-ui\\src\\test\\resources").listFiles();
-//        for (File file : files) {
-//            if (file.isFile()) {
-//                if(file.getName().endsWith(".html")){
-//                    results.add(file.getName());
-//                }
-//            }
-//        }
-//        System.out.println(results);
-//    }
 }
