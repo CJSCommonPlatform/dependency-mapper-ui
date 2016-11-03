c2b2d1fd2b2ad583a5f8e0cff0b68a59a8d1a4ff
Chin Perera <chinthanaperera@gmail.com>
2016-11-03 10:50:18 +0000

    Chin Perera :  RAML doc viewer

--- src/main/resources/static/js/render/bindings/displayRamlEvent.js
+++ src/main/resources/static/js/render/bindings/displayRamlEvent.js
@@ -1,7 +1,12 @@
-define([], function() {
-    return function(e){
-        var microserviceName = e.data.node.label;
-
-        $("#ramlDetails").load("/ramlReport?ramlFileName=" + microserviceName + ".html .row");
+define([], function () {
+    return function (e) {
+        var microserviceName = e.data.node.label,
+            ramlFileName = "";
+        if (microserviceName != "") {
+            ramlFileName = microserviceName.replace(" ", "-");
+            $("#ramlDetails").empty().load("/ramlReport?ramlFileName=" + ramlFileName + ".html .row");
+        } else {
+            $("#ramlDetails").empty().html("<h1>No RAML Document found</h1>");
+        }
     }
-})
+});
