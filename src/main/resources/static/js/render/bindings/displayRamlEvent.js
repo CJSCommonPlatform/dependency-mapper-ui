cbf4e6bca942cca8b1282545318ab8f99d591ff6
David Simons <david.simons@hmcts.net>
2016-11-04 11:09:49 +0000

    Actually commit graph viz trial

--- src/main/resources/static/js/render/bindings/displayRamlEvent.js
+++ src/main/resources/static/js/render/bindings/displayRamlEvent.js
@@ -6,10 +6,24 @@ define([], function () {
         e.data.renderer.render();
 
         if (microserviceName != "") {
-            var ramlFileName = microserviceName.replace(" ", "-");
-            $("#ramlDetails").empty().load("/ramlReport?ramlFileName=" + ramlFileName + ".html .row");
-        } else {
-            $("#ramlDetails").empty().html("<h1>No RAML Document found</h1>");
+           var ramlFileName = microserviceName.replace(" ", "-");
+
+            $.ajax({
+                statusCode: {
+                    500: function () {
+                        $("#ramlDetails").empty().html("<h1>No RAML document found</h1>");
+                    }
+                },
+                url:"/ramlReport?ramlFileName=" + ramlFileName + ".html .row",
+                async: false,
+                success: function(data){
+                    if(data.response == "success"){
+                        $("#ramlDetails").empty().load("/ramlReport?ramlFileName=" + ramlFileName + ".html .row");
+                    }else{
+                        console.log("Ajax call failed to the file on the file systme")
+                    }
+                }
+            });
         }
     }
 });
