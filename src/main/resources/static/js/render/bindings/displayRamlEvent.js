342a4fb6b513f2ddd58bfb8c1edb92eff6b40826
Chin Perera <chinthanaperera@gmail.com>
2016-11-07 12:14:40 +0000

    Chin Perera :  Handling server 500 error on RAML docs

--- src/main/resources/static/js/render/bindings/displayRamlEvent.js
+++ src/main/resources/static/js/render/bindings/displayRamlEvent.js
@@ -4,9 +4,23 @@ define([], function () {
             ramlFileName = "";
         if (microserviceName != "") {
             ramlFileName = microserviceName.replace(" ", "-");
-            $("#ramlDetails").empty().load("/ramlReport?ramlFileName=" + ramlFileName + ".html .row");
-        } else {
-            $("#ramlDetails").empty().html("<h1>No RAML Document found</h1>");
+
+            $.ajax({
+                statusCode: {
+                    500: function () {
+                        $("#ramlDetails").empty().html("<h1>No RAML document found</h1>");
+                    }
+                },
+                url:"/ramlReport?ramlFileName=" + ramlFileName + ".html .row",
+                async: false,
+                success: function(data){
+                    if(data.response == "success"){
+                        $("#ramlDetails").empty().load("/ramlReport?ramlFileName=" + ramlFileName + ".html .row");
+                    }else{
+                        console.log("Ajax call failed to the file on the file systme")
+                    }
+                }
+            });
         }
     }
 });
